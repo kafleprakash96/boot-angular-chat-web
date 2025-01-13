86217c3daf492e17e2e539e66482d607e4608042
Prakash Kafle <kafleprakash96@gmail.com>
2025-01-13 14:46:18 -0600

    Initial commit

---
+++ chat-web-backend/src/main/java/com/prkcode/chatwebbackend/ChatWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prkcode.chatwebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWebBackendApplication.class, args);
+	}
+
+}
---
+++ chat-web-backend/src/test/java/com/prkcode/chatwebbackend/ChatWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prkcode.chatwebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
