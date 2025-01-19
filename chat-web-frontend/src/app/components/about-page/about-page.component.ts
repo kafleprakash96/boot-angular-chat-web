cc6a408871e765dbc267ec84b5a5879d54d6f325
Prakash Kafle <kafleprakash96@gmail.com>
2025-01-19 00:36:29 -0600

    Update about page

--- chat-web-frontend/src/app/components/about-page/about-page.component.ts
+++ chat-web-frontend/src/app/components/about-page/about-page.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
+import { MatFormField, MatInputModule } from '@angular/material/input';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-about-page',
   imports: [MatCardModule,MatInputModule,
-    MatButtonModule,ReactiveFormsModule],
+    MatButtonModule,ReactiveFormsModule,MatFormField,MatGridListModule,MatIconModule],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.css'
 })
