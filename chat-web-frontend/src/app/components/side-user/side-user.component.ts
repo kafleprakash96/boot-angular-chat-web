7f36958ea35114841ad878d681b5d84660219627
Prakash Kafle <kafleprakash96@gmail.com>
2025-01-20 19:51:51 -0600

    Initialized side-user,user and user-dialog component

---
+++ chat-web-frontend/src/app/components/side-user/side-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-user',
+  imports: [],
+  templateUrl: './side-user.component.html',
+  styleUrl: './side-user.component.css'
+})
+export class SideUserComponent {
+
+}
